e332039dd2348a914257e35a9bad8a1a0fff7e0b
Juliano Padilha <sistemas.padilha@gmail.com>
2016-09-24 11:42:39 -0300

    Mentoring javascript

--- portfolio/assets/js/functions.js
+++ portfolio/assets/js/functions.js
@@ -1,9 +1,43 @@
-$(window).scroll(function(){
-	youtubeVidScroll();
+$(function() {
+	mentoringBubbleClick();
 });
 
-function youtubeVidScroll(){
+function mentoringBubbleClick() {
+	$('.face').on('click', function() {
+		var $this = $(this),
+			faceTop = $this.position().top,
+			vertMath = -1 * (faceTop - 230);
+
+		console.log(vertMath);
+
+		$this.parent().css('top', + vertMath +'px');
+
+		$(this).addClass('has-bubble-open')
+		.siblings().removeClass('has-bubble-open');
+	});
+
+	
+}
+
+
+$(window).scroll(function() {
+	youtubeVidScroll();
+	startMentoring();
+});
+
+function youtubeVidScroll() {
 	var wScroll = $(window).scrollTop();
 
 	$('.video-strip').css('background-position', 'center -'+ wScroll +'px');
+}
+
+function startMentoring() {
+	var wScroll = $(window).scrollTop();
+
+	if($('section.mentoring').offset().top  - 500 < wScroll) {
+		$('.faces').addClass('launched');
+		setTimeout(function(){
+			$('.face:nth-child(3)').addClass('has-bubble-open');
+		}, 300);
+	}
 }
