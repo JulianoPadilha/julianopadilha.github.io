eea13bd051cdf01e9a0c29f98f6540f516ce8f49
Juliano Padilha <sistemas.padilha@gmail.com>
2016-10-30 19:42:02 -0200

    Muda configuração dos diretórios

--- blog/_site/gv/js/app.js
+++
@@ -1,7 +0,0 @@
-$('.banner').backstretch("images/background-full.jpg");
-$('.topCategories').backstretch("images/background-grava.jpg");
-$(document).ready(function(){
-	$('.navIcon').click(function(){
-		$('nav').stop().slideToggle('fast');						 
-	});					   
-});
